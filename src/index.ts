eaf7a5a3f0f581de70baf78a18a31c19fd6c89b1
Mr. Dolch <dolch@nice-technologies.com>
2017-05-27 20:46:58 +0200

    Samples removed and CouchdbModule introduced

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,8 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpModule } from '@angular/http';
 
-import { SampleComponent } from './sample.component';
-import { SampleDirective } from './sample.directive';
-import { SamplePipe } from './sample.pipe';
-import { SampleService } from './sample.service';
 import { CouchdbService, CouchdbDoc, CouchdbListEntry } from './couchdb.service';
 
-export * from './sample.component';
-export * from './sample.directive';
-export * from './sample.pipe';
-export * from './sample.service';
 export * from './couchdb.service';
 
 @NgModule({
@@ -19,23 +11,19 @@ export * from './couchdb.service';
     CommonModule, HttpModule
   ],
   declarations: [
-    CouchdbDoc, CouchdbListEntry,
-    SampleComponent,
-    SampleDirective,
-    SamplePipe
+    CouchdbDoc,
+    CouchdbListEntry,
   ],
   exports: [
-    CouchdbDoc, CouchdbListEntry,
-    SampleComponent,
-    SampleDirective,
-    SamplePipe
+    CouchdbDoc,
+    CouchdbListEntry,
   ]
 })
-export class SampleModule {
+export class CouchdbModule {
   static forRoot(): ModuleWithProviders {
     return {
-      ngModule: SampleModule,
-      providers: [SampleService, CouchdbService]
+      ngModule: CouchdbModule,
+      providers: [ CouchdbService ]
     };
   }
 }
